59cd6081c9ae6801a9e1134a6fe88494a168017b
Grant Ho <55564015+gho19@users.noreply.github.com>
2020-06-27 16:17:51 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var canvas = document.getElementById("draw");
+
+var ctx = canvas.getContext("2d");
+resize();
+
+// resize canvas when window is resized
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+
+// initialize position as 0,0
+var pos = { x: 0, y: 0};
+
+// new position from mouse events
+function setPosition(e) {
+  pos.x = e.clientX;
+  pos.y = e.clientY;
+}
+
+function draw(e) {
+  if (e.buttons !== 1) return; // if mouse is not clicked, do not go further
+
+  var color = document.getElementById("color").value;
+
+  ctx.beginPath(); // begin the drawing path
+
+  ctx.lineCap = "round"; // rounded end cap
+  ctx.strokeStyle = color; // hex color of line
+
+  ctx.moveTo(pos.x, pos.y); // from position
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // to position
+
+  ctx.stroke(); // draw it!
+}
+
+
+function size(e) {
+  ctx.lineWidth = 3; // width of line
+  while (e.keyCode > 0) {
+    if (e.keyCode == 38) {
+      ctx.lineWidth+=2;
+      return;
+    }
+    else if (e.keyCode == 40) {
+      ctx.lineWidth--;
+      return;
+    }
+    else {
+      return;
+    }
+  }
+}
+
+
+
+// add window event listener to trigger when window is resized
+window.addEventListener("resize", resize);
+
+// add event listeners to trigger on different mouse events
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
+document.addEventListener("keyup", size);
